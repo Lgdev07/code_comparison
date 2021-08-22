0ea31a67e7469507041c60b741a9a80c1bd71a58
Luan Gomes <luan_saopaulo06@hotmail.com>
2021-08-22 16:19:32 -0300

    change host

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :code_comparison, CodeComparisonWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "codecomparison.me", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   check_origin: [
     "//codecomparison.me",
