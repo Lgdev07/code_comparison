6dfa49e9ed5deb416dfe68554596577a405fd2d0
Luan Gomes <luan_saopaulo06@hotmail.com>
2021-08-22 16:03:09 -0300

    add origin from custom domain and force ssl

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,12 @@ use Mix.Config
 config :code_comparison, CodeComparisonWeb.Endpoint,
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  check_origin: ["https://dreary-sore-eland.gigalixirapp.com"]
+  check_origin: [
+    "//codecomparison.me",
+    "//www.codecomparison.me",
+    "//dreary-sore-eland.gigalixirapp.com"
+  ],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
